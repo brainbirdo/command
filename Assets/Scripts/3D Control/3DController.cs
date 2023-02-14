bd82d9c7cbee5a9b4494da8069b5bb6fc84dc7cf
Bethany Rainbird <ayresii.art@gmail.com>
2023-02-14 16:19:30 +0000

    Environment Built

---
+++ Assets/Scripts/3D Control/3DController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 3DController : MonoBehaviour
+{
+    public bool isViewing;
+    public bool StatView;
+    public bool MapView;
+
+public void Update()
+{
+
+}
+
+public OnTriggerEnter
+{
+    //switch cam
+    isViewing = true;
+}
+
+public void StatsView()
+{
+
+}
+
+}
